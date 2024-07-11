d818869461f3ed568981686cdd1969fc62ea2428
omer-io <omer.jamil@rakurai.io>
2024-07-11 15:14:36 +0500

    hello

--- repo_auto_update.sh
+++ repo_auto_update.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+echo -e "\nEnter local repo path"
+read repo_path
+cd "$repo_path"
+
 # print repo status
 echo -e "\nThese changes are going to be pushed to remote repo:\n"
 git status
 
+echo -e "\nEnter file names to stage or press y to stage all"
+read 
 # read commit message from user
 echo -e "\nEnter commit message: "
 read commit_msg
