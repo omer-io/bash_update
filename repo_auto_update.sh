97d6d130126b3afc35b03514856a2c393e2a0a60
omer-io <omer.jamil@rakurai.io>
2024-07-11 16:36:53 +0500

    it is what it is

--- repo_auto_update.sh
+++ repo_auto_update.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-echo -e "\nEnter local repo path"
-read repo_path
-cd "$repo_path"
+# read local repo path from user
+#echo -e "\nEnter local repo path"
+#read repo_path
+#cd "$repo_path" || exit
 
 # print repo status
-echo -e "\nThese changes are going to be pushed to remote repo:\n"
-git status
+#echo -e "\nThese files are modified/untracked/deleted:\n"
+#changed_files=$(git status --porcelain | sed s/^...//)
+#echo "$changed_files"
 
-echo -e "\nEnter file names to stage or press y to stage all"
-read 
+#wc -l "$changed_files"
+
+#echo -e "\nEnter file names to stage seperated by space or press y to stage all"
+#read files
+#echo "$files" | awk '{print $1, $2, $3}'
+
 # read commit message from user
 echo -e "\nEnter commit message: "
 read commit_msg
